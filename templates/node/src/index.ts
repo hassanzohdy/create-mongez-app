afc447f2b376b83fad5d2f1da3b31353aaf8d7fe
hasan <hassanzohdy@gmail.com>
2022-10-27 08:40:26 +0200

    "Updated Dependencies"

--- templates/node/src/index.ts
+++ templates/node/src/index.ts
@@ -1,6 +1,6 @@
-import { rootPath } from '@mongez/node';
+import { rootPath } from "@mongez/node";
 
 // get the absolute path to the root of the project for the given path
-const root = rootPath('');
+const root = rootPath("");
 
-console.log('Hello, World, you are in the root of the project:', root);
+console.log("Project has been started in:", root);
