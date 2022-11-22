8517416d0072f3335daef07d6acbf06420be7279
hasan <hassanzohdy@gmail.com>
2022-11-22 10:12:11 +0200

    "Fixes"

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,4 +1,9 @@
-import * as path from "path";
+import path from "path";
+
+if (typeof global.__dirname === "undefined") {
+  // as __dirname is not defined in esm modules, we need to define it manually
+  global.__dirname = path.resolve();
+}
 
 export function template(templateName: string): string {
   return path.resolve(__dirname, "../", "../", "templates", templateName);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import * as path from "path";
 import createNewApp from "./commands/create-new-app";
 import print, { colors } from "./helpers/cli";
 
+if (typeof global.__dirname === "undefined") {
+  // as __dirname is not defined in esm modules, we need to define it manually
+  global.__dirname = path.resolve();
+}
+
 export default function createApp() {
   const packageJson: any = getJson(
     path.resolve(__dirname, "../", "package.json")
