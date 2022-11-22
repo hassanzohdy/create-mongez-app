27a6dfb38559661fe2d5dd0e14ae7f2bebfc6e46
hasan <hassanzohdy@gmail.com>
2022-11-22 10:18:35 +0200

    "Fixes"

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,14 +1,9 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-if (typeof global.__dirname === "undefined") {
-  // as __dirname is not defined in esm modules, we need to define it manually
+const __filename = fileURLToPath(import.meta.url);
 
-  const __filename = fileURLToPath(import.meta.url);
-
-  // 👇️ "/home/john/Desktop/javascript"
-  global.__dirname = path.dirname(__filename);
-}
+const __dirname = path.dirname(__filename);
 
 export function template(templateName: string): string {
   return path.resolve(__dirname, "../", "../", "templates", templateName);
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,9 @@ import { fileURLToPath } from "url";
 import createNewApp from "./commands/create-new-app";
 import print, { colors } from "./helpers/cli";
 
-if (typeof global.__dirname === "undefined") {
-  // as __dirname is not defined in esm modules, we need to define it manually
+const __filename = fileURLToPath(import.meta.url);
 
-  const __filename = fileURLToPath(import.meta.url);
-
-  // 👇️ "/home/john/Desktop/javascript"
-  global.__dirname = path.dirname(__filename);
-}
+const __dirname = path.dirname(__filename);
 
 export default function createApp() {
   const packageJson: any = getJson(
