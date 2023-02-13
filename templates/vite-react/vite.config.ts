d4815e8f9420caa9b0666ec1bd08e3ab5c7b8eef
hasan <hassanzohdy@gmail.com>
2023-02-13 22:30:57 +0200

    "Updating dependencies"

--- templates/vite-react/vite.config.ts
+++ templates/vite-react/vite.config.ts
@@ -1,11 +1,18 @@
 import mongezVite from "@mongez/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig, UserConfigExport } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), mongezVite()],
+    plugins: [
+      svgr({
+        exportAsDefault: false,
+      }),
+      mongezVite(),
+      react(),
+    ],
     envPrefix: "APP_",
   } as UserConfigExport;
 });
