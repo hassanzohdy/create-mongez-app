b2a41830451942633df56a90b688d44d73a6fce3
Hasan Zohdy <hassanzohdy@gmail.com>
2023-07-08 04:29:38 +0300

    "Fixed templates"

--- templates/vite-react-headless-ui/src/shared/definitions.d.ts
+++ templates/vite-react-headless-ui/src/shared/definitions.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite-plugin-svgr/client" />
-import "@emotion/react";
-import type { MantineTheme } from "@mantine/core";
-
-declare module "@emotion/react" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends MantineTheme {}
-}
