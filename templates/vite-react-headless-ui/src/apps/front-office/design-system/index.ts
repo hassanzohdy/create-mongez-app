8f66540551491fbfc42b3acb00ccda8bfbede8b2
hassanzohdy <hassanzohdy@gmail.com>
2023-08-23 00:18:48 +0300

    "Updated deps and added Huskier"

--- templates/vite-react-headless-ui/src/apps/front-office/design-system/index.ts
+++ templates/vite-react-headless-ui/src/apps/front-office/design-system/index.ts
@@ -1,9 +1,9 @@
 import { googleFont } from "@mongez/dom";
 import { current } from "@mongez/react";
-import "./main.css";
+import "./main.scss";
 
 if (current("localeCode") === "ar") {
-  // load Cairo font from google
+  // load Cairo font from google for Arabic language
   googleFont(
     "https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap",
   );
